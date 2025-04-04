c1dfaad6dcb3effc7894ce0550c2ba1279bc4f72
forprem <forprem@gmail.com>
2025-04-04 19:42:14 -0400

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Could not find 'root' element. Make sure index.html has a <div id='root'>.");
+}
